0c9d2e039898dd2a19486ba8b492e2acd6607546
Tanaka <t.khondowe@gmail.com>
2022-11-25 01:13:43 -0600

    User + Job models

--- server.js
+++ server.js
@@ -3,13 +3,14 @@ import dotenv from "dotenv";
 
 const app = express();
 dotenv.config();
-const port = process.env.PORT || 5000;
+const port = 5000;
 
 // db connection
 import connectDB from "./db/connectdb.js";
 
 // routers and routes
-import authRouter from "./routes/authRoutes.js"
+import authRouter from "./routes/authRoutes.js";
+import jobsRouter from "./routes/jobRoutes.js";
 app.use(express.json())
 
 app.get("/", (req, res) => {
@@ -17,6 +18,7 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/jobs", jobsRouter);
 
 // middleware
 import errorHandlerMiddleware from "./middleware/error-handler.js";
