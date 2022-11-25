874b507f45b1001283895abb9653a6681eaffc16
Tanaka <t.khondowe@gmail.com>
2022-11-25 00:55:23 -0600

    auth routes structure

---
+++ server.js
@@ -0,0 +1,40 @@
+import express from "express";
+import dotenv from "dotenv";
+
+const app = express();
+dotenv.config();
+const port = process.env.PORT || 5000;
+
+// db connection
+import connectDB from "./db/connectdb.js";
+
+// routers and routes
+import authRouter from "./routes/authRoutes.js"
+app.use(express.json())
+
+app.get("/", (req, res) => {
+  res.send("This is the server talking");
+});
+
+app.use("/api/v1/auth", authRouter);
+
+// middleware
+import errorHandlerMiddleware from "./middleware/error-handler.js";
+import notFoundMiddleware from "./middleware/not-found.js";
+// check for incorrect routes
+app.use(notFoundMiddleware);
+// check errors in existing routes (bottom of all middleware)
+app.use(errorHandlerMiddleware);
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL);
+    app.listen(port, () => {
+      console.log(`Server listening at port ${port} right now`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start()
