6d35d95efc853ba60387c3a7806bfce2feff7df5
Tanaka <t.khondowe@gmail.com>
2022-11-25 13:08:40 -0600

    User validation error

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,5 +1,20 @@
-const errorHandlerMiddleware = (err, req, res, next) => {
-  res.status(500).json({ msg: err});
-}
+import { StatusCodes } from "http-status-codes";
 
+const errorHandlerMiddleware = (err, req, res, next) => {
+  console.log(err);
+  const defaultError = {
+    statusCode: StatusCodes.INTERNAL_SERVER_ERROR,
+    msg: "Something went wrong",
+  };
+
+  if (err.name === "ValidationError") {
+    defaultError.statusCode = StatusCodes.BAD_REQUEST;
+    // defaultError.msg = err.message;
+    defaultError.msg = Object.values(err.errors)
+      .map((item) => item.message)
+      .join(",");
+  }
+  res.status(defaultError.statusCode).json({ msg: defaultError.msg });
+};
+ 0
 export default errorHandlerMiddleware;
