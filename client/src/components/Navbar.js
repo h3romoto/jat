d05642459011460fd6075128fcf3de88bcd2c8cd
Tanaka <t.khondowe@gmail.com>
2022-11-26 02:15:34 -0600

    Navbar structure

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,12 +1,46 @@
 import Wrapper from "../assets/wrappers/Navbar";
-import {FaHome} from "react-icons/fa";
+import { FaAlignLeft, FaUserCircle, FaCaretDown } from "react-icons/fa";
+import useAppContext from "../context/appContext";
+import Logo from "./Logo";
 
 const Navbar = () => {
   return (
     <Wrapper>
-      <h4>navbar</h4>
-      <FaHome />
+      <div className="nav-center">
+        <button
+          className="toggle-btn"
+          onClick={() => {
+            console.log("toggle sidebar");
+          }}
+        >
+          <FaAlignLeft />
+        </button>
+        <div>
+          <Logo />
+          <h3 className="logo-text">Dashboard</h3>
+        </div>
+        <div className="btn-container">
+          <button
+            type="button"
+            className="btn"
+            onClick={() => console.log("Show/hide dropdown")}
+          >
+            <FaUserCircle/>
+            Tanaka
+            <FaCaretDown />
+          </button>
+          <div className="dropdown show-dropdown">
+            <button
+              type="button"
+              className="dropdown-btn"
+              onClick={() => console.log("Logout user")}
+            >
+              logout
+            </button>
+          </div>
+        </div>
+      </div>
     </Wrapper>
-  )
-}
+  );
+};
 export default Navbar;
