55a58e2d3706dc2dac0370da5d06a094f0afc7aa
Tanaka <t.khondowe@gmail.com>
2022-11-26 20:02:06 -0600

    Job container

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -254,7 +254,7 @@ const AppProvider = ({ children }) => {
       })
     } catch (error) {
       console.log(error.response)
-      logoutUser()
+      // logoutUser()
     }
     clearAlert()
   }
