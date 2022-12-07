553ddae0f81dbcaafe477082d8f85aca6cc09764
Tanaka <t.khondowe@gmail.com>
2022-12-06 20:00:52 -0600

    Basic pagination backend done

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -54,6 +54,16 @@ const getAllJobs = async (req, res) => {
     result = result.sort('-position')
   }
 
+
+  
+  // pagination
+  const page = Number(req.query.page) || 1
+  const limit = Number(req.query.limit) || 10
+  const skip = (page - 1) * limit //10
+  result = result.skip(skip).limit(limit)
+
+  result = result.skip(skip).limit(limit)
+
   // chain sort conditions
   const jobs = await result;
 
