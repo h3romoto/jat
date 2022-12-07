e3ee323482bc2e9ef0d404f5971f33dbcd9671d1
Tanaka <t.khondowe@gmail.com>
2022-12-06 20:18:00 -0600

    Calculate number of pages for pagination

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -54,22 +54,22 @@ const getAllJobs = async (req, res) => {
     result = result.sort('-position')
   }
 
-
-  
   // pagination
   const page = Number(req.query.page) || 1
   const limit = Number(req.query.limit) || 10
   const skip = (page - 1) * limit //10
-  result = result.skip(skip).limit(limit)
 
   result = result.skip(skip).limit(limit)
 
   // chain sort conditions
   const jobs = await result;
+  const totalJobs = await Job.countDocuments(queryObject)
+  console.log(totalJobs)
+  const numOfPages = Math.ceil(totalJobs / limit)
 
   res
     .status(StatusCodes.OK)
-    .json({ jobs, totalJobs: jobs.length, numOfPages: 1 });
+    .json({ jobs, totalJobs: jobs.length, numOfPages: numOfPages });
 };
 
 const updateJob = async (req, res) => {
