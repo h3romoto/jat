5e97e52c2a2efe98ab7d89ec33a5f152362cd83f
Tanaka <t.khondowe@gmail.com>
2022-12-06 21:12:17 -0600

    Prev and next page

--- client/src/components/PageBtnContainer.js
+++ client/src/components/PageBtnContainer.js
@@ -11,10 +11,19 @@ const PageBtnContainer = () => {
   });
 
   const prevPage = () => {
-    console.log("prev page");
+    let newPage = page - 1;
+    if (newPage < 1) {
+      newPage = numOfPages;
+    }
+    changePage(newPage);
   };
+
   const nextPage = () => {
-    console.log("next page");
+    let newPage = page + 1;
+    if (newPage > numOfPages) {
+      newPage = 1;
+    }
+    changePage(newPage);
   };
 
   return (
@@ -27,14 +36,14 @@ const PageBtnContainer = () => {
       <div className="btn-container">
         {pages.map((pageNumber) => {
           return (
-              <button
-                type='button'
-                className={pageNumber === page ? 'pageBtn active' : 'pageBtn'}
-                key={pageNumber}
-                onClick={() => changePage(pageNumber)}
-              >
-                {pageNumber}
-              </button>
+            <button
+              type="button"
+              className={pageNumber === page ? "pageBtn active" : "pageBtn"}
+              key={pageNumber}
+              onClick={() => changePage(pageNumber)}
+            >
+              {pageNumber}
+            </button>
           );
         })}
       </div>
