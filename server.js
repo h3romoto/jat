539380b80094fd5233669413c5bd040c68df2bff
Tanaka <t.khondowe@gmail.com>
2022-12-08 00:06:30 -0600

    direct get routes from server to client

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ import express from "express";
 import dotenv from "dotenv";
 import "express-async-errors";
 import morgan from "morgan";
+
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+import path from 'path'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
 const app = express();
 dotenv.config();
 const port = process.env.PORT;
@@ -13,12 +20,16 @@ import connectDB from "./db/connectdb.js";
 import authRouter from "./routes/authRoutes.js";
 import jobsRouter from "./routes/jobRoutes.js";
 app.use(express.json());
-
+app.use(express.static(path.resolve(__dirname, './client/build')))
 app.get("/api/v1", (req, res) => { res.json({ MSG: "This is the server talking" }); });
 
 // routes
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/jobs", authenticateUser, jobsRouter);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './client/build', 'index.html'))
+})
 
 // middleware
 import errorHandlerMiddleware from "./middleware/error-handler.js";
